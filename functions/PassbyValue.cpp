59e6447a630ef37528b620780251251217dc7eac
Bangash1122 <ubaidkhan0077800@gmail.in>
2026-08-20 19:30:42 -0400

    passbyValue to  commit this change

---
+++ functions/PassbyValue.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// Pass by value example
+using namespace std;
+
+void changeX(int x) {
+    x = 2*x;
+    cout << "* =" << x << endl;
+}
+
+int main(){
+    int x = 5;
+    cout << "x =" << x << endl;
+    changeX(x);
+    cout << "x =" << x << endl;
+
+    return 0;
+}
+
+//outpot first value is
+//x =5
+//x =10
