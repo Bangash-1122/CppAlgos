40bb5b144eade3713d4ec2d0c5f5bf3daa655670
Ubaid_Bangash <ubaidkhan0077800@gmail.com>
2026-09-17 18:17:22 -0400

    Add vectorsSize.cpp to demonstrate vector usage

---
+++ Arrays/vectorsSize.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec; //0
+
+    cout << "size = " << vec.size() << endl;
+    vec.push_back(25);
+    cout << "after push back size =" << vec.size() endl;
+
+    for(int val : vec) {
+        cout << val << endl;
+    }
+    return 0;
+}
+
+//size = 0
+//After push back size = 1
+// then 25
