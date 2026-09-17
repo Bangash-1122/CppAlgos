b5604daaa48a51a8d4c1d2232c2d2bed3eee49af
Ubaid_Bangash <ubaidkhan0077800@gmail.com>
2026-09-17 19:39:58 -0400

    Add solution for finding single number in array
    Implement solution for LeetCode problem #136 to find the single number in an array.

---
+++ Arrays/SingleNumArray.cpp
@@ -0,0 +1,34 @@
+// ****
+// Leet Code  problem # 136 'easy'
+// ****
+// Single Number
+// Given a non-empty array of integers nums, every element appears twice except for one. Find the single one
+//  You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for (int val : nums) {
+            ans = ans ^ val;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> nums = {4, 1, 2, 1, 2};
+
+    Solution obj;
+
+    cout << obj.singleNumber(nums) << endl;
+
+    return 0;
+}
